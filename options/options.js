ce7b7af3206b00aedbed0395a5db7fcd99df6329
Crowdbotics <team@crowdbotics.com>
2021-09-03 00:57:45 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "morning_salad_30289Identifier",
+  url: "https://morning_salad_30289Identifier.botics.co",
+  api: "https://morning_salad_30289Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
